7fd2eac3f876b2c900376ec17d2b8cf4f6a5ef9a
Unknown <123leej@naver.com>
2018-07-12 00:50:28 +0900

    feat: decode Frame Packet (LorawanFrameDecoder)

--- src/decode-lorawan-frame.c
+++ src/decode-lorawan-frame.c
@@ -10,15 +10,12 @@
 #include "decode-lorawan-frame.h"
 
 
-static void DecodeLoraWanFrameHdrs(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, uint8_t *pkt, uint16_t len, PacketQueue *pq)
-{
-    return;
-}
 
 static int DecodeLORAWANFrameControls(ThreadVars *tv, Packet *p, uint8_t *pkt, uint16_t len)
 {
     return 0;
 }
+
 
 static int DecodeLORAWANFrameOptions(ThreadVars *tv, Packet *p, uint8_t *pkt, uint16_t len)
 {
@@ -27,6 +24,32 @@ static int DecodeLORAWANFrameOptions(ThreadVars *tv, Packet *p, uint8_t *pkt, ui
 
 static int DecodeLORAWANFramePacket(ThreadVars *tv, Packet *p, uint8_t *pkt, uint16_t len)
 {
+    if (len < LORAWAN_FRAME_HEADER_LEN_MIN) {
+        DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
+        return -1;
+    }
+
+    p->lorafh = (LorawanFrame4Hdr *)pkt;
+
+    if (LORAWAN_FRAME_GET_HLEN(p) < LORAWAN_FRAME_GET_FOPTS_LEN + 7) {
+        DECODER_SET_EVENT(p,LORAWAN_FRAME_HLEN_TOO_SMALL);
+        return -1;
+    }
+
+    if (len != LORAWAN_FRAME_GET_LEN(p)) {
+        DECODER_SET_EVENT(p,LORAWAN_FRAME_PKT_INVALID);
+        return -1;
+    }
+
+    DecodeLORAWANFrameControls(tv, p, pkt + sizeof(p->lorawan_frame_hdr.dev_addr), sizeof(p->lorawan_frame_hdr.fctl));
+
+    if (p->lorawanhdr.fctl.fopts_len > 0) {
+        DecodeLORAWANFrameOptions(tv, p, pkt + sizeof(p->lorawan_frame_hdr.dev_addr) + sizeof(p->lorawan_frame_hdr.fctl->fopts_len), p->lorawan_frame_hdr.fctl->fopts_len);
+    }
+
+    p->payload = pkt + LoRAWAN_FRAME_HEADER_LEN;        //??????????????????????
+    p->payload_len = len - LoRAWAN_FRAME_HEADER_LEN;
+
     return 0;
 }
 
