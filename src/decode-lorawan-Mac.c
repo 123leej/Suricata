cd3e3f2dcad70ec0a64d998d54a9b6e980972366
Unknown <shdmsqkd123@naver.com>
2018-07-16 22:43:09 +0900

    lorawanMac Macro definition
    fixed decoding logics, added Macro def. in header

--- src/decode-lorawan-Mac.c
+++ src/decode-lorawan-Mac.c
@@ -12,35 +12,15 @@
 static int DecodeLorawanMACPacket(ThreadVars *tv, Packet *p, uint8_t *pkt, uint16_t len)
 {
 
-	if (len < LORAWAN_MAC_HEADER_LEN){
-		DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
+	if (len != LORAWAN_MAC_HEADER_LEN){
+		DECODER_SET_EVENT(p,LORAWAN_HEADER_INVALID_LEN);
 		return -1;
 	}
 
 	p->lorawan_mac_header = (LorawanMacHdr *)pkt;
 
-	if(len < LORAWAN_MAC_GET_HEADER_LEN(p)) {
-		DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
-		return -1;
-	}
+	p->lorawan_mac_vars.macpayload = pkt + LORAWAN_MAC_HEADER_LEN;
 
-	LORAWAN_MAC_SET_MTYPE(p);
-	LORAWAN_MAC_SET_RFU(p);
-	LORAWAN_MAC_SET_MAJOR(p);
-
-	p->lorawan_mac_vars.macpayload = pkt + LORAWAN_MAC_GET_HEADER_LEN(p);
-
-	if(len < LORAWAN_MAC_GET_HEADER_LEN(p) + LORAWAN_MAC_PAYLOAD_LEN_MIN) {
-		DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
-		return -1;
-	}
-
-	if(len != LORAWAN_MAC_GET_LEN(p)) {
-		DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
-		return -1;
-	}
-
-	//TODO trim MIC 
 	return 0;
 }
 
@@ -57,23 +37,26 @@ static int DecodeLorawanMAC(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, ui
         return;
     }
 
+	if(len < LORAWAN_MAC_HEADER_LEN + LORAWAN_MAC_PAYLOAD_LEN_MIN) {
+		DECODER_SET_EVENT(p,LORAWAN_PKT_TOO_SMALL);
+		return -1;
+	}
 
-    switch (LORAWAN_MAC_GET_MTYPE(p)) {
-/*    	case 0x00: //join-request
-    		break;
-    	case 0x01: //join-accept
-    		break;
-*/
+	LORAWAN_MAC_TRIM_MIC(p,p->lorawan_mac_vars.payload);
+
+    switch (p->lorawan_mac_header.mtype) {
+
     	//TODO check for uplink and downlink about detailed MAC command
-    	case 0x02: //unconfirmed data up
+    	case 0x02:	//unconfirmed data up
     		break;
-    	case 0x04: //unconfirmed data down
+    	case 0x04:	//unconfirmed data down
     		break;
-    	case 0x05: //confirmed data up
+    	case 0x05:	//confirmed data up
     		break;
-    	case 0x06: //confirmed data down
+    	case 0x06:	//confirmed data down
     		break;
-    	default:
+    	default:	//join-request, join-accept, RFU, proprietary
+    		//TODO whether goto veridict directly?
     		break;
     }
 }
